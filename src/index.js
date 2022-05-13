b6c639fe5c80d952958e12bc3f9f707c7264f318
Vitor Ferraz Sandes <sandesvitor@gmail.com>
2022-05-13 10:35:01 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
-const express = require("express");
-const { getRepoEnvironments } = require("./github.js")
-const { Octokit } = require("octokit");
-require('dotenv').config();
+const { App, createNodeMiddleware } = require("@octokit/app");
 
-const app = express();
+const app = new App({
+    appId: 123,
+    privateKey: "-----BEGIN PRIVATE KEY-----\n...",
+    oauth: {
+        clientId: "0123",
+        clientSecret: "0123secret",
+    },
+    webhooks: {
+        secret: "secret",
+    },
+});
 
-app.get("/environments", async (req, res) => {
-    try {
-        const repository = process.env.REPOSITORY;
-        const owner = process.env.OWNER;
-        const token = process.env.GITHUB_TOKEN;
-        const octokit = new Octokit({ auth: token });
-        const environments = await getRepoEnvironments(repository, owner, octokit)
-        console.log(environments)
+const { data } = await app.octokit.request("/app");
+console.log("authenticated as %s", data.name);
 
-        res.json(environments);
-    } catch (e) {
-        console.log(e)
+for await (const { octokit, repository } of app.eachRepository.iterator()) {
+    await octokit.request("POST /repos/{owner}/{repo}/dispatches", {
+        owner: repository.owner.login,
+        repo: repository.name,
+        event_type: "my_event",
+    });
+}
+
+app.webhooks.on("issues.opened", async ({ octokit, payload }) => {
+await octokit.request(
+    "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
+    {
+    owner: payload.repository.owner.login,
+    repo: payload.repository.name,
+    issue_number: payload.issue.number,
+    body: "Hello World!",
     }
-})
+);
+});
 
-app.listen(5050, () => {
-    console.log("Server running on port 5050");
-})
+app.oauth.on("token", async ({ token, octokit }) => {
+const { data } = await octokit.request("GET /user");
+console.log(`Token retrieved for ${data.login}`);
+});
+
+require("http").createServer(createNodeMiddleware(app)).listen(3000);
+// can now receive requests at /api/github/*
