8265d70eed1c03d7871acbb33ac0d60533af0039
Vitor Ferraz Sandes <sandesvitor@gmail.com>
2022-05-16 15:23:03 -0300

    Fix

--- src/github.js
+++ src/github.js
@@ -6,7 +6,8 @@ async function getRepoEnvironments(repository, owner, octokit){
         .then(res => res.data.environments)
 
     return environments;
-    // da
+    // das
+    
 }
 
 module.exports = {
