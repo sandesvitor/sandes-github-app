39cab0a6aad11dfbb2fe250794cf227da9ddee23
Vitor Ferraz Sandes <sandesvitor@gmail.com>
2022-05-16 16:33:34 -0300

    Fix

--- src/github.js
+++ src/github.js
@@ -6,7 +6,7 @@ async function getRepoEnvironments(repository, owner, octokit){
         .then(res => res.data.environments)
 
     return environments;
-    // AGORA VAI 3
+    // AGORA VAI 2
 
 }
 
