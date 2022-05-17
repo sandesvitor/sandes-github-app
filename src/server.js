9995a2f7a848512b1da5faef632b5f16acd11c4c
Vitor Ferraz Sandes <sandesvitor@gmail.com>
2022-05-17 16:49:24 -0300

    Fix

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.get("/environments", async (req, res) => {
     try {
         // OUTRA MUDANÇAd
-        // DELETE sdddsdd
+        // DELETE sdddsddd
         const repository = process.env.REPOSITORY;
         const owner = process.env.OWNER;
         const token = process.env.GITHUB_TOKEN;
